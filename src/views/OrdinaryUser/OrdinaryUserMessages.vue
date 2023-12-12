40a113090cccb1f506912fca632fe2e6fe0c189b
Laverne24 <1974996288@qq.com>
2023-12-12 14:24:22 +0800

    实现了不太完善CRUD,目前只能用运行状态单条件查询

--- src/views/OrdinaryUser/OrdinaryUserMessages.vue
+++ src/views/OrdinaryUser/OrdinaryUserMessages.vue
@@ -1,17 +1,53 @@
 <template>
+  <div>
+    <el-button @click="openDialog" type="primary">新增</el-button>
+
+    <el-dialog v-model="dialogVisible" title="提示" width="30%" :before-close="handleClose">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
+
+
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  data(){
-    return{
-      list:['普通用户:我的设备','普通用户:我的消息','普通用户:我的订单']
-    }
+  setup() {
+    const dialogVisible = ref(false);
+    const formData = ref({
+      serial_number: null,
+      device_name: null,
+      location_id: null,
+      owner_id: 12,
+      status: null,
+      device_model: null,
+      purchase_date: null,
+      warranty_time: 365,
+    });
+
+    const openDialog = () => {
+      dialogVisible.value = true;
+    };
+
+    const handleClose = () => {
+      // Your before-close logic here
+    };
+
+    return {
+      dialogVisible,
+      openDialog,
+      handleClose,
+      formData,
+    };
   },
-  mounted(){
-    this.$emit('send',this.list)
-  }
-}
+};
 </script>
 
 <style>
