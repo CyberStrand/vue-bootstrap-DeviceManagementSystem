d2b40d16575c4112ffacc1c893d5b6de8d46c93b
Laverne24 <1974996288@qq.com>
2023-12-22 19:40:18 +0800

    实现了普通用户待办事项页面的增删查改导印统序

--- src/views/OrdinaryUser/OrdinaryUserToDo.vue
+++ src/views/OrdinaryUser/OrdinaryUserToDo.vue
@@ -6,19 +6,17 @@
       <el-main>
         <!--选择栏-->
         <div class="search-bar">
-          <el-button style="float:left" type="warning" @click="openDialog"><el-icon><CirclePlus /></el-icon>&nbsp;新增</el-button>
+          <el-button style="float:left" type="warning" @click="fetchDevice"><el-icon><CirclePlus /></el-icon>&nbsp;查询所有</el-button>
+          <el-button style="float:left" type="success" @click="openDialog"><el-icon><CirclePlus /></el-icon>&nbsp;新增</el-button>
           <el-button style="float:left" type="success" @click="printBox"><el-icon><Printer /></el-icon>&nbsp;打印</el-button>
-          <el-button style="float:left" type="info" @click="printBox"><el-icon><Promotion /></el-icon>&nbsp;导出</el-button>
-          <el-button style="float:left" type="info" @click="printBox"><el-icon><PieChart /></el-icon>&nbsp;统计</el-button>
-          <el-button style="float:left" type="info" @click="printBox"><el-icon><SortUp /></el-icon>&nbsp;排序</el-button>
-          <el-button style="float:left" type="info" @click="printBox"><el-icon><SortDown /></el-icon>&nbsp;排序</el-button>
-          <el-button style="float:right" type="primary" @click="query"><el-icon><Search /></el-icon>&nbsp;查询</el-button>
+          <el-button style="float:left" type="success" @click="clickExport"><el-icon><Promotion /></el-icon>&nbsp;导出</el-button>
+          <el-button style="float:left" type="success" @click="statistics"><el-icon><PieChart /></el-icon>&nbsp;统计</el-button>
+          <el-button style="float:left" type="info" @click="Sort"><el-icon><SortUp /></el-icon>&nbsp;倒序查看</el-button>
+          <el-button style="float:right" type="primary" @click="fetchTodo"><el-icon><Search /></el-icon>&nbsp;查询</el-button>
           <el-select style="float:right" class="search-input" v-model="todoStatus" placeholder="状态">
             <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
           </el-select>
-
         </div>
-
         <!--数据表-->
         <div id="1">
         <el-table :data="tableData"
@@ -34,13 +32,12 @@
           </el-table-column>
           <el-table-column fixed="right" label="操作">
             <template v-slot="scope">
-              <el-button size="mini" type="primary" @click="handleEdit(scope.row.todoId)"><el-icon><EditPen /></el-icon>&nbsp;修改</el-button>
-              <el-button size="mini" type="danger" @click="handleDelete(scope.row.todoId)"><el-icon><DeleteFilled /></el-icon>&nbsp;删除</el-button>
+              <el-button size="small" type="primary" @click="handleEdit(scope.row)"><el-icon><EditPen /></el-icon>修改</el-button>
+              <el-button size="small" type="danger" @click="handleDelete(scope.row.todoId)"><el-icon><DeleteFilled /></el-icon>&nbsp;删除</el-button>
             </template>
           </el-table-column>
         </el-table>
         </div>
-
         <!--分页栏-->
         <div class="pagination-bar">
           <el-pagination
@@ -53,10 +50,7 @@
               @current-change="handleCurrentChange"
           />
         </div>
-
-
-        <el-dialog
-            title="新增代办实现"
+        <el-dialog title="新增待办事项"
             v-model="dialogVisible"
             width="30%"
             :before-close="handleClose"
@@ -70,9 +64,7 @@
             </el-form-item>
           </el-form>
         </el-dialog>
-
-        <el-dialog
-            title="编辑"
+        <el-dialog title="编辑待办事项"
             v-model="EditDialogVisible"
             width="30%"
             :before-close="handleClose"
@@ -81,14 +73,35 @@
             <el-form-item label="待办内容" prop="todoContent">
               <el-input v-model="formData.todoContent"></el-input>
             </el-form-item>
-            <el-form-item label="待办事项状态" prop="todoStatus">
-              <el-input v-model="formData.todoStatus"></el-input>
+            <el-form-item label="已完成？">
+              <el-switch v-model="formData.todoBoolStatus" />
             </el-form-item>
             <el-form-item>
               <el-button type="primary" @click="updateDevice">保存</el-button>
             </el-form-item>
           </el-form>
         </el-dialog>
+        <el-dialog title="导出待办事项"
+                   v-model="exportDialogVisible"
+                   width="30%"
+                   :before-close="handleClose">
+          <el-switch
+              v-model="exportAll"
+              class="ml-2"
+              inline-prompt
+              style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+              active-text="导出所有"
+              inactive-text="导出当前"
+          />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <el-button type="primary" @click="exportTodoList"> &nbsp;导出</el-button>
+        </el-dialog>
+        <el-dialog title="统计待办事项"
+                   v-model="statisticDialogVisible"
+                   width="50%"
+                   :before-close="handleClose">
+        &nbsp; <div id="chart" style="height: 300px;"></div>
+        </el-dialog>
+
 
       </el-main>
     </el-container>
@@ -98,21 +111,37 @@
 <script>
 import {ref, onMounted, toRaw} from 'vue';
 import print from 'print-js'
+import {export_json_to_excel} from "@/vendor/Export2Excel";
+import {
+  CirclePlus,
+  DeleteFilled, EditPen,
+  PieChart,
+  Printer,
+  Promotion,
+  Search,
+  SortDown,
+  SortUp
+} from "@element-plus/icons-vue";
+import * as echarts from 'echarts'
 const apiHeaders = {
   'Content-Type': 'application/json',
   'Authorization': "Bearer "+localStorage.getItem("token") // Replace with your actual JWT token
 };
 export default {
   name: 'OrdinaryUserTodo',
-  setup(props, { root }) {
+  components: {EditPen, Promotion, SortUp, Search, SortDown, DeleteFilled, PieChart, Printer, CirclePlus},
+  setup() {
     const dialogVisible = ref(false);
+    const exportDialogVisible = ref(false);
+    const statisticDialogVisible = ref(false);
     const tableData = ref([]);
     const total = ref(0);
     const pageNum = ref(1);
     const pageSize = ref(5);
-    const todoStatus = ref('');
+    const todoStatus = ref(null);
     const ownerId = ref(24);
     const todoId = ref(null);
+    const exportAll = ref(true);
     const options = ref([
       { value: 'done', label: '已完成' },
       { value: 'undone', label: '未完成' },
@@ -123,22 +152,62 @@ export default {
       user_id:1054,
       todoContent:'',
       todoStatus:'undone',
+      todoBoolStatus:false,
     });
 
-    const handleSizeChange = (pagesize) => {
-      pageSize.value = pagesize;
-      fetchDevice();
+    const handleSizeChange = (page_size) => {
+      console.log("执行了handleSizeChange函数");
+      pageSize.value = page_size;
+      fetchTodo();
     };//修改每一页展示的记录数目
-    const handleCurrentChange = (pagenum) => {
-      pageNum.value = pagenum;
-      fetchDevice();
+    const handleCurrentChange = (page_num) => {
+      console.log("执行了handleCurrentChange函数");
+      pageNum.value = page_num;
+      fetchTodo();
     };//修改页码
+    const fetchTodo = () =>{
+      let todo_status = todoStatus.value
+      console.log("执行了fetchTodo函数");
+      if (todo_status==null){
+        console.log("没有进行条件查询，查询所有函数")
+        fetch(`http://localhost:8080/ordinaryUser/todos?pageNum=${pageNum.value}&pageSize=${pageSize.value}`, {
+          method: 'POST',
+          headers: apiHeaders,
+        })
+            .then(res => res.json())
+            .then(res => {
+              tableData.value = res.data.list;
+              total.value = res.data.total;
+            })
+            .catch(error => {
+              console.error('获取数据失败:', error);
+            });
+      }
+      else{
+          console.log("在条件查询")
+          console.log(todoStatus)
+          fetch(`http://localhost:8080/ordinaryUser/todoSelect`, {
+            method: 'POST',
+            headers: apiHeaders,
+            body: JSON.stringify({
+              "todoStatus": todoStatus.value,
+              "pageNum": pageNum.value,
+              "pageSize": pageSize.value,
+            })
+          })
+              .then(res => res.json())
+              .then(res => {
+                tableData.value = res.data.list;
+                total.value = res.data.total;
+              })
+              .catch(error => {
+                console.error('获取数据失败:', error);
+              });
+        }
+      }
     const fetchDevice = () => {
-      console.log("开始获取数据")
-      console.log(pageNum.value)
-      console.log("开始获取数据")
-      console.log(pageSize.value)
-      console.log("开始获取数据")
+      todoStatus.value = null;
+      console.log("执行了fetchDevice函数");
       fetch(`http://localhost:8080/ordinaryUser/todos?pageNum=${pageNum.value}&pageSize=${pageSize.value}`, {
         method: 'POST',
         headers: apiHeaders,
@@ -152,26 +221,8 @@ export default {
             console.error('获取数据失败:', error);
           });
     };//（查）
-    const query = () => {
-      fetch(`http://localhost:8080/ordinaryUser/todosSelect`, {
-        method: 'POST',
-        headers: apiHeaders,
-        body: JSON.stringify({
-          "todoStatus": todoStatus.value,
-          "pageNum": pageNum.value,
-          "pageSize": pageSize.value,
-        })
-      })
-          .then(res => res.json())
-          .then(res => {
-            tableData.value = res.data.list;
-            total.value = res.data.total;
-          })
-          .catch(error => {
-            console.error('获取数据失败:', error);
-          });
-    };//根据设备状态查询设备信息（查）
     const handleDelete = (todoId) => {
+      console.log("执行了handleDelete函数");
       fetch(`http://localhost:8080/ordinaryUser/todo?todoId=${todoId}`, {
         method: 'DELETE',
         headers: apiHeaders,
@@ -188,17 +239,24 @@ export default {
             console.error('删除失败', error.message);
           });
     };//删除设备（删）
-    const handleEdit =(todo_id) =>{
+    const handleEdit =(row) =>{
+      console.log("执行了handleEdit函数");
       EditDialogVisible.value = true;
-      todoId.value = todo_id;
+      todoId.value = row.todoId;
+      formData.value.todoContent = row.todoContent;
+      formData.value.todoStatus = row.todoStatus;
+      formData.value.todoBoolStatus = formData.value.todoStatus === "done";
     };
     const openDialog = () => {
+      console.log("执行了openDialog函数");
       dialogVisible.value = true;
     };//打开添加设备对话框
     const handleClose = (done) => {
+      console.log("执行了handleClose函数");
       done();
     };//关闭添加设备对话框
     const saveData = () => {
+      console.log("执行力saveData函数")
       console.log(formData.value)
       console.log(toRaw(formData.value))
       fetch("http://localhost:8080/ordinaryUser/todoModify",{
@@ -210,16 +268,20 @@ export default {
           .then(data => {
             console.log('Response from server:', data);
             dialogVisible.value = false;// 关闭对话框
-            fetchDevice();//自动刷新
+            fetchTodo();//自动刷新
           })
           .catch(error => {
             console.error('Error during data submission:', error);
           });
     };//保存添加设备对话框中的数据
     const updateDevice = () =>{
+      console.log("执行了updateDevice函数");
       formData.value.todoId = todoId.value;
+      console.log(formData.value.todoBoolStatus);
+      if(formData.value.todoBoolStatus) formData.value.todoStatus = 'done'
+      else formData.value.todoStatus = 'undone'
       console.log(formData.value)
-      console.log(toRaw(formData.value))
+
       fetch("http://localhost:8080/ordinaryUser/todo",{
         method:'PUT',
         headers:apiHeaders,
@@ -229,13 +291,14 @@ export default {
           .then(data => {
             console.log('Response from server:', data);
             dialogVisible.value = false;// 关闭对话框
-            fetchDevice();//自动刷新
+            fetchTodo();//自动刷新
           })
           .catch(error => {
             console.error('Error during data submission:', error);
           });
     }
     const getStatusLabel = (status) => {
+      //console.log("执行了getStatusLabel函数");
       const statusMap = {
         'done': '已完成',
         'undone': '未完成',
@@ -243,10 +306,12 @@ export default {
       return statusMap[status] || '';
     };
     const getStatusColor = (status) => {
+     // console.log("执行了getStatusColor函数");
       if(status==='undone')return '#ff7b7b'
       else if(status==='done')return '#5b952a'
     };
     const printBox = () => {
+      console.log("执行了printBox函数");
       setTimeout(() => {
         // 在这里使用 this.$print
         print({
@@ -256,8 +321,141 @@ export default {
           targetStyles: ['*']
         });
       }, 500);
+    };//打印当页的数据
+    const clickExport = () =>{
+      exportDialogVisible.value = true;
+    };//弹出导出的对话框
+    const exportTodoList = ()=>{
+      if (exportAll.value === true) exportAllTodo();
+      else if (exportAll.value===false) exportTodo();
+    } //选择导出什么数据
+    const exportTodo = () => {
+      console.log("执行了exportTodo函数");
+      fetch(`http://localhost:8080/ordinaryUser/todos?pageNum=${pageNum.value}&pageSize=${pageSize.value}`, {
+        method: 'POST',
+        headers: apiHeaders,
+      })
+          .then(res => res.json())
+          .then(res => {
+            const todoList = res.data.list;
+            console.log(res.data.list);
+            const tHeader = ['待办内容', '状态'];
+            const filterVal = ['todoContent', 'todoStatus'];
+            const data = formatJson(filterVal, todoList);
+            export_json_to_excel(tHeader, data, '本页待办事项');
+          });
+    };//导出当页的数据
+    const exportAllTodo = () => {
+      console.log("执行了exportAllTodo函数");
+      fetch(`http://localhost:8080/ordinaryUser/allTodos`, {
+        method: 'GET',
+        headers: apiHeaders,
+      })
+          .then(res => res.json())
+          .then(res => {
+            console.log(res);
+            const todoList = res.data;
+            console.log(res.data);
+            const tHeader = ['待办内容', '状态'];
+            const filterVal = ['todoContent', 'todoStatus'];
+            const data = formatJson(filterVal, todoList);
+            export_json_to_excel(tHeader, data, '所有待办事项');
+          });
+    };//导出所有的数据
+    const formatJson = (filterVal, jsonData) => {
+      console.log("执行了formatJson函数");
+      return jsonData.map(v => filterVal.map(j => v[j]));
+    };
+    const Sort = () =>{
+      console.log("执行了Sort函数");
+      fetch(`http://localhost:8080/ordinaryUser/sortdown?pageNum=${pageNum.value}&pageSize=${pageSize.value}`, {
+        method: 'POST',
+        headers: apiHeaders,
+      })
+          .then(res => res.json())
+          .then(res => {
+            tableData.value = res.data.list;
+            total.value = res.data.total;
+          })
+          .catch(error => {
+            console.error('获取数据失败:', error);
+          });
+    };
+
+    const statistics = () => {
+      console.log("执行了Statistics函数");
+      statisticDialogVisible.value = true;
+      fetchStatistics();
+    };
+    const fetchStatistics = () => {
+      fetch(`http://localhost:8080/ordinaryUser/statistic`, {
+        method: 'GET',
+        headers: apiHeaders,
+      })
+          .then(res => res.json())
+          .then(res => {
+            console.log('Raw response:', res);
+
+            // 添加映射关系将标签转化为文字
+            const data = res.data.map(item => ({ value: item.count, name: mapStatus(item.todo_status) }));
+
+            // 绘制饼状图
+            drawPieChart(data);
+          })
+          .catch(error => {
+            console.error('获取统计数据失败:', error);
+          });
+    };
+
+// 添加一个映射函数
+    const mapStatus = (status) => {
+      const statusMap = {
+        'done': '已完成',
+        'undone': '未完成',
+      };
+      return statusMap[status] || '';
+    };
+
+    const drawPieChart = (data) => {
+      // 使用 ECharts 绘制饼状图
+      const chart = echarts.init(document.getElementById('chart'));
+      const option = {
+        title: {
+          text: '待办事项统计',
+          subtext: '已完成和未完成数量',
+          left: 'center',
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)',
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: data.map(item => item.name),
+        },
+        series: [
+          {
+            name: '数量',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: data,
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        ],
+      };
+
+      chart.setOption(option);
     };
     onMounted(() => {
+      console.log("执行了onMounted函数");
       fetchDevice();
     });
 
@@ -274,20 +472,29 @@ export default {
       formData,
       todoId,
       EditDialogVisible,
+      exportDialogVisible,
+      statisticDialogVisible,
+      exportAll,
       getStatusColor,
       getStatusLabel,
       updateDevice,
       handleEdit,
       openDialog,
       fetchDevice,
+      fetchTodo,
       handleSizeChange,
       handleCurrentChange,
       handleDelete,
-      query,
       handleClose,
       saveData,
       printBox,
-
+      Sort,
+      exportTodo,
+      formatJson,
+      statistics,
+      exportAllTodo,
+      clickExport,
+      exportTodoList,
     };
   },
 };
@@ -304,10 +511,6 @@ export default {
   margin-right: 10px;
 }
 
-.action-buttons {
-  padding: 10px 0;
-}
-
 .pagination-bar {
   padding: 10px 0;
   display: flex;
@@ -318,13 +521,6 @@ export default {
   text-align: center;
 }
 
-.el-dialog {
-  overflow-y: auto;
-}
-
-.error-status {
-  color: #ff7b7b; /* Light red */
-}
 h2{
   color: rgb(64, 158, 255);
 }
